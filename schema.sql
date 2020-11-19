d84ee023123ef4cadb9df2bfc0d0f199ffffb266
ahchambers <66989949+ahchambers@users.noreply.github.com>
2020-11-19 11:20:50 +1100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,77 +1,72 @@
-DROP TABLE IF EXISTS "Pop_100_Web";
-DROP TABLE IF EXISTS "Pop_100_API";
-DROP TABLE IF EXISTS "Top_250_Web";
-DROP TABLE IF EXISTS "Top_250_API"; 
-DROP TABLE IF EXISTS "Genre_Top";
-DROP TABLE IF EXISTS "Genre_Pop";
+DROP TABLE IF EXISTS Pop_100_Web, Top_250_Web, Pop_100_API, Top_250_API, Genre_Top, Genre_Pop;
 
-CREATE TABLE "Pop_100_Web" (
-    "Popularity_Rank" INT   NOT NULL,
-    "Title" VARCHAR   NOT NULL,
-    "Rating" FLOAT   NOT NULL,
-    CONSTRAINT "pk_Pop_100_Web" PRIMARY KEY (
-        "Popularity_Rank"
-     )
+CREATE TABLE Pop_100_Web (
+    Popularity_Rank INT   NOT NULL UNIQUE,
+    Title VARCHAR   NOT NULL UNIQUE,
+    Rating FLOAT   NOT NULL,
+	PRIMARY KEY (Title)
 );
-CREATE TABLE "Top_250_Web" (
-    "Rating_Rank" INT   NOT NULL,
-    "Title" VARCHAR   NOT NULL,
-    "Rating" FLOAT   NOT NULL,
-    CONSTRAINT "pk_Top_250_Web" PRIMARY KEY (
-        "Rating_Rank"
-     )
+	
+CREATE TABLE Top_250_Web (
+    Rating_Rank INT   NOT NULL UNIQUE,
+	Title VARCHAR   NOT NULL UNIQUE,
+    Rating FLOAT   NOT NULL,
+	PRIMARY KEY (Title)
 );
-CREATE TABLE "Pop_100_API" (
-    "Title" VARCHAR   NOT NULL,
-    FOREIGN KEY ("Popularity_Rank") REFERENCES "Pop_100_Web" ("Popularity_Rank"),
+
+CREATE TABLE Pop_100_API (
+    Title VARCHAR   NOT NULL,
     "Year" INT   NOT NULL,
-    "Rated" VARCHAR   NOT NULL,
-    "Released" DATE   NOT NULL,
-    "Runtime" INT   NOT NULL,
-    "Director" VARCHAR   NOT NULL,
-    "Plot" VARCHAR   NOT NULL,
-    "Poster" VARCHAR   NOT NULL,
-    "Metascore" INT   NOT NULL,
-    "imdbID" VARCHAR   NOT NULL,
-    "imdbRating" FLOAT   NOT NULL,
-    "imdbVotes" INT   NOT NULL,
+    Rated VARCHAR   NOT NULL,
+    Released DATE   NOT NULL,
+    Runtime INT   NOT NULL,
+    Director VARCHAR   NOT NULL,
+    Plot VARCHAR   NOT NULL,
+    Poster VARCHAR   NOT NULL,
+    Metascore INT   NOT NULL,
+    imdbID VARCHAR   NOT NULL UNIQUE,
+    imdbRating FLOAT   NOT NULL,
+    imdbVotes INT   NOT NULL,
     "Type" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Pop_100_API" PRIMARY KEY (
-        "imdbID"
-     )
+	Popularity_Rank INT NOT NULL,
+    PRIMARY KEY (imdbID),
+	FOREIGN KEY (Popularity_Rank) REFERENCES Pop_100_Web (Popularity_Rank)
 );
-CREATE TABLE "Top_250_API" (
-    "Title" VARCHAR   NOT NULL,
-    FOREIGN KEY ("Rating_Rank") REFERENCES "Top_250_Web" ("Rating_Rank"),
+
+CREATE TABLE Top_250_API (
+    Title VARCHAR   NOT NULL,
     "Year" INT   NOT NULL,
-    "Rated" VARCHAR   NOT NULL,
-    "Released" DATE   NOT NULL,
-    "Runtime" INT   NOT NULL,
-    "Director" VARCHAR   NOT NULL,
-    "Plot" VARCHAR   NOT NULL,
-    "Poster" VARCHAR   NOT NULL,
-    "Metascore" INT   NOT NULL,
-    "imdbID" VARCHAR   NOT NULL,
-    "imdbRating" FLOAT   NOT NULL,
-    "imdbVotes" INT   NOT NULL,
+    Rated VARCHAR   NOT NULL,
+    Released DATE   NOT NULL,
+    Runtime INT   NOT NULL,
+    Director VARCHAR   NOT NULL,
+    Plot VARCHAR   NOT NULL,
+    Poster VARCHAR   NOT NULL,
+    Metascore INT   NOT NULL,
+    imdbID VARCHAR   NOT NULL UNIQUE,
+    imdbRating FLOAT   NOT NULL,
+    imdbVotes INT   NOT NULL,
     "Type" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Top_250_API" PRIMARY KEY (
-        "imdbID"
-     )
+	Rating_Rank INT NOT NULL,
+    PRIMARY KEY (imdbID),
+	FOREIGN KEY (Rating_Rank) REFERENCES Top_250_Web (Rating_Rank)
 );
-CREATE TABLE "Genre_Top" (
-    FOREIGN KEY ("imdbID") REFERENCES "Top_250_API" ("imdbID"),
-    FOREIGN KEY ("Title") REFERENCES "Top_250_Web" ("Title"),
-    "Genre" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Genre_Top" PRIMARY KEY (
-        "imdbID"
-     )
+
+CREATE TABLE Genre_Top (
+    FOREIGN KEY (imdbID) REFERENCES Top_250_API (imdbID),
+    FOREIGN KEY (Title) REFERENCES Top_250_Web (Title),
+    Genre VARCHAR   NOT NULL UNIQUE,
+	imdbID VARCHAR   NOT NULL,
+	Title  VARCHAR   NOT NULL,
+    PRIMARY KEY (imdbID)
 );
-CREATE TABLE "Genre_Pop" (
-    FOREIGN KEY ("imdbID") REFERENCES "Pop_100_API" ("imdbID"),
-    FOREIGN KEY ("Title") REFERENCES "Pop_100_Web" ("Title"),
-    FOREIGN KEY ("Genre") REFERENCES "Genre_Top" ("Genre"),
-    CONSTRAINT "pk_Genre_Pop" PRIMARY KEY (
-        "imdbID"
-     )
+
+CREATE TABLE Genre_Pop (
+    FOREIGN KEY (imdbID) REFERENCES Pop_100_API (imdbID),
+    FOREIGN KEY (Title) REFERENCES Pop_100_Web (Title),
+    FOREIGN KEY (Genre) REFERENCES Genre_Top (Genre),
+	Genre VARCHAR   NOT NULL,
+	imdbID VARCHAR   NOT NULL,
+	Title  VARCHAR   NOT NULL,
+    PRIMARY KEY (imdbID)
 );
