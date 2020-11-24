fe172bac80176272ab11909c26418c0060d40864
Matt Taylor <matt_taylor14@hotmail.co.uk>
2020-11-24 23:16:57 +1100

    update

--- Database/schema.sql
+++ Database/schema.sql
@@ -2,19 +2,20 @@ DROP TABLE IF EXISTS Pop_100_Web, Top_250_Web, Pop_100_API, Top_250_API, Genre_T
 
 CREATE TABLE Pop_100_Web (
     Popularity_Rank INT   NOT NULL UNIQUE,
-    Title VARCHAR   NOT NULL UNIQUE,
-    Rating FLOAT   NOT NULL,
-	PRIMARY KEY (Title)
+    Title VARCHAR   NOT NULL,
+    Rating FLOAT   ,
+	PRIMARY KEY (Popularity_Rank)
 );
 	
 CREATE TABLE Top_250_Web (
     Rating_Rank INT   NOT NULL UNIQUE,
-	Title VARCHAR   NOT NULL UNIQUE,
+	Title VARCHAR   NOT NULL,
     Rating FLOAT   NOT NULL,
-	PRIMARY KEY (Title)
+	PRIMARY KEY (Rating_Rank)
 );
 
 CREATE TABLE Pop_100_API (
+    Popularity_Rank INT NOT NULL,
     Title VARCHAR   NOT NULL,
     "Year" INT   NOT NULL,
     Rated VARCHAR   NOT NULL,
@@ -26,14 +27,14 @@ CREATE TABLE Pop_100_API (
     Metascore INT   NOT NULL,
     imdbID VARCHAR   NOT NULL UNIQUE,
     imdbRating FLOAT   NOT NULL,
-    imdbVotes INT   NOT NULL,
+    imdbVotes INT   ,
     "Type" VARCHAR   NOT NULL,
-	Popularity_Rank INT NOT NULL,
     PRIMARY KEY (imdbID),
 	FOREIGN KEY (Popularity_Rank) REFERENCES Pop_100_Web (Popularity_Rank)
 );
 
 CREATE TABLE Top_250_API (
+    Rating_Rank INT NOT NULL,
     Title VARCHAR   NOT NULL,
     "Year" INT   NOT NULL,
     Rated VARCHAR   NOT NULL,
@@ -45,28 +46,24 @@ CREATE TABLE Top_250_API (
     Metascore INT   NOT NULL,
     imdbID VARCHAR   NOT NULL UNIQUE,
     imdbRating FLOAT   NOT NULL,
-    imdbVotes INT   NOT NULL,
+    imdbVotes INT   ,
     "Type" VARCHAR   NOT NULL,
-	Rating_Rank INT NOT NULL,
     PRIMARY KEY (imdbID),
 	FOREIGN KEY (Rating_Rank) REFERENCES Top_250_Web (Rating_Rank)
 );
 
 CREATE TABLE Genre_Top (
+    id INT PRIMARY KEY,
     FOREIGN KEY (imdbID) REFERENCES Top_250_API (imdbID),
-    FOREIGN KEY (Title) REFERENCES Top_250_Web (Title),
-    Genre VARCHAR   NOT NULL UNIQUE,
-	imdbID VARCHAR   NOT NULL,
+    imdbID VARCHAR   NOT NULL,
 	Title  VARCHAR   NOT NULL,
-    PRIMARY KEY (imdbID)
+    Genre VARCHAR   NOT NULL
 );
 
 CREATE TABLE Genre_Pop (
+    id INT PRIMARY KEY,
     FOREIGN KEY (imdbID) REFERENCES Pop_100_API (imdbID),
-    FOREIGN KEY (Title) REFERENCES Pop_100_Web (Title),
-    FOREIGN KEY (Genre) REFERENCES Genre_Top (Genre),
-	Genre VARCHAR   NOT NULL,
-	imdbID VARCHAR   NOT NULL,
+    imdbID VARCHAR   NOT NULL,
 	Title  VARCHAR   NOT NULL,
-    PRIMARY KEY (imdbID)
+    Genre VARCHAR   NOT NULL
 );
